82e7ba827a2a232b86d72a4b9fb2a28b8922d51e
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-31 21:02:14 +0530

    feat: add Firebase admin and client initialization files

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCznQVypanpIhiU3aJlfW_rtn3yAQo-l-8",
+  authDomain: "vinterra-700f9.firebaseapp.com",
+  projectId: "vinterra-700f9",
+  storageBucket: "vinterra-700f9.firebasestorage.app",
+  messagingSenderId: "933286910544",
+  appId: "1:933286910544:web:8f7243d4eea2765ee44f55",
+  measurementId: "G-R8EY2WY48K",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
