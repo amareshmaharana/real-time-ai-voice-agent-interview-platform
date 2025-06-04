8eef535bb50500cbbccc3ee8e4f1131ec087b65c
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-04 14:24:50 +0530

    fix: update Firebase configuration to use environment variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,16 +2,15 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// -> Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // firebase api key
-  authDomain: "vinterra-700f9.firebaseapp.com",
-  projectId: "vinterra-700f9",
-  storageBucket: "vinterra-700f9.firebasestorage.app",
-  messagingSenderId: "933286910544",
-  appId: "1:933286910544:web:8f7243d4eea2765ee44f55",
-  measurementId: "G-R8EY2WY48K",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // firebase api key
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
