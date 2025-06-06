d659977e3584f7ddd711459c04dcf34b0c1d09cb
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-06 17:42:42 +0530

    configured

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,5 +1,5 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
-import {getAuth} from "firebase-admin/auth";
+import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
@@ -7,18 +7,18 @@ const initFirebaseAdmin = () => {
 
   if (!apps.length) {
     initializeApp({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        })
-    })
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
   }
 
   return {
     auth: getAuth(),
     db: getFirestore(),
-  }
+  };
 };
 
 export const { auth, db } = initFirebaseAdmin();
